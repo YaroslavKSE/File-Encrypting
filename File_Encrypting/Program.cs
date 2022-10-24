3182cf40ffd5242fc33364aad9824e3d38c29e4d
YaroslavKSE <ikhomych@kse.org.ua>
2022-10-24 15:34:00 +0300

    Added "secret mode"

--- File_Encrypting/Program.cs
+++ File_Encrypting/Program.cs
@@ -6,7 +6,8 @@ const string path = @"C:\C#\File_Encrypting\File_Encrypting\";
 while (true)
 {
     Console.WriteLine("Enter '1' to encrypt \n" +
-                      "Enter '2' to decrypt:");
+                      "Enter '2' to decrypt: \n" +
+                      "Enter '3' to continue in secret mode");
     var whatToDo = Console.ReadLine();
     var caesarCypher = new Caesar();
     if (whatToDo == "1")
@@ -25,7 +26,7 @@ while (true)
         var fileWriter = new FileWriter();
         fileWriter.Write($"{path}{forWriting}", encrypted);
     }
-    else
+    if(whatToDo == "2")
     {
         Console.WriteLine("Enter file name for decryption:");
         var forReading = Console.ReadLine();
@@ -40,6 +41,23 @@ while (true)
 
         var fileWriter = new FileWriter();
         fileWriter.Write($"{path}{forWriting}", decrypted);
+    }
+    else
+    {
+        Console.WriteLine("Enter file name for encryption:");
+        var forReading = Console.ReadLine();
+
+        Console.WriteLine("Enter file name for saving:");
+        var forWriting = Console.ReadLine();
+
+        var key = Random.Shared.Next(100);
+        
+        var fileReader = new FileReader();
+        var encrypted = caesarCypher.Encrypt(fileReader.Read($"{path}{forReading}"), key);
+
+        var fileWriter = new FileWriter();
+        fileWriter.Write($"{path}{forWriting}", encrypted);
+
     }
 
     Console.WriteLine("Successful");
